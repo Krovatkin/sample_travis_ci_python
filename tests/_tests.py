88cf0f6235737bd1594b197ffbfb82914df3b2fb
Nick Korovaiko <villedepommes@fb.com>
2019-07-27 20:22:20 -0700

    introduce a failure

--- tests/_tests.py
+++ tests/_tests.py
@@ -5,7 +5,7 @@ class AdderTestCase(unittest.TestCase):
 
 
     def test_add_one(self):
-        self.assertEqual(adder.add.add_one(0), 1)
+        self.assertEqual(adder.add.add_one(0), 2)
         self.assertEqual(adder.add.add_one(1), 2)
 
 if __name__ == '__main__':
