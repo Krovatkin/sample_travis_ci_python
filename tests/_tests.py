1e9801e7ff7a6b6ee8ab6f26db5d9b0aed7be825
Nick Korovaiko <villedepommes@fb.com>
2019-07-29 00:08:24 -0700

    add a new test case

--- tests/_tests.py
+++ tests/_tests.py
@@ -7,6 +7,7 @@ class AdderTestCase(unittest.TestCase):
     def test_add_one(self):
         self.assertEqual(adder.add.add_one(0), 1)
         self.assertEqual(adder.add.add_one(1), 2)
+        self.assertEqual(adder.add.add_one(2), 3)
 
 if __name__ == '__main__':
     unittest.main()
